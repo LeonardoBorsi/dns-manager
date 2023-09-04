e2242e049cb6416c2301e8713e4b253e025ec662
leonardo_borsi <l.borsi@hu-care.com>
2023-09-04 21:47:27 +0200

    validation on IP and Domain Names

--- domains/forms.py
+++ domains/forms.py
@@ -1,13 +1,32 @@
 from django import forms
 from .models import Domain, SubDomain, TLD
+from django.core.validators import validate_ipv4_address, RegexValidator, ValidationError
 
+def validate_domain_names(value):
+    domain_name_validator = RegexValidator(
+        r'^[a-zA-Z0-9]([a-zA-Z0-9\-]{0,61}[a-zA-Z0-9])?$',
+        'Invalid domain name.'
+    )
+    
+    names = value.split(";")
+    
+    for name in names:
+        try:
+            domain_name_validator(name)
+        except ValidationError:
+            raise ValidationError("Invalid domain names")
+    
 class DomainForm(forms.ModelForm):
+    ip_address = forms.CharField(validators=[validate_ipv4_address])
+    names = forms.CharField(validators=[validate_domain_names])
     class Meta:
         model = Domain
         fields = ['ip_address', 'names', 'TLD']
 
 
 class SubDomainForm(forms.ModelForm):
+    ip_address = forms.CharField(validators=[validate_ipv4_address])
+    names = forms.CharField(validators=[validate_domain_names])
     class Meta:
         model = SubDomain
         fields = ['ip_address', 'names']
